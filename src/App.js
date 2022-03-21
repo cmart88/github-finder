8b7b9557625844994214919187cbd7ca206787b6
Cmart88 <ftballplaya1221@gmail.com>
2022-03-21 05:33:58 -0700

    single user route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,38 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import User from "./pages/User";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <div className='flex flex-col justify-between  '>
-          <Navbar />
-          <main className=' mx-auto px-40 pb-12 h-100 '>
-            <Routes>
-              <Route path='/' element={<Home />}></Route>
-              <Route path='/about' element={<About />}></Route>
-              <Route path='/contact' element={<Contact />}></Route>
-              <Route path='/*' element={<NotFound />}></Route>
-            </Routes>
-          </main>
-        </div>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between  '>
+            <Navbar />
 
-        <Footer />
-      </Router>
+            <main className=' mx-auto px-40 pb-12 h-100 '>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/about' element={<About />}></Route>
+                <Route path='/user/:login' element={<User />}></Route>
+                <Route path='/contact' element={<Contact />}></Route>
+                <Route path='/*' element={<NotFound />}></Route>
+              </Routes>
+            </main>
+          </div>
+
+          <Footer />
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -3,14 +3,24 @@ import alertReducer from "./alertReducer";
 
 const AlertContext = createContext();
 
-export const AlertProvider = ({ childern }) => {
+export const AlertProvider = ({ children }) => {
   const initialState = null;
 
   const [state, dispatch] = useReducer(alertReducer, initialState);
 
+  // Set an alert
+  const setAlert = (msg, type) => {
+    dispatch({
+      type: "SET_ALERT",
+      payload: { msg, type },
+    });
+
+    setTimeout(() => dispatch({ type: "REMOVE_ALERT" }), 3000);
+  };
+
   return (
-    <AlertContext.Provider value={{ alert: state }}>
-      {childern}
+    <AlertContext.Provider value={{ alert: state, setAlert }}>
+      {children}
     </AlertContext.Provider>
   );
 };
