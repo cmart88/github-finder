3967acee44d18e6ac7cfbe41a3c167e893758c3e
Cmart88 <ftballplaya1221@gmail.com>
2022-04-03 09:21:16 -0700

    home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import UserSearch from "../components/users/UserSearch";
 function Home() {
   return (
     <div>
+      <h1 className='text-4xl mb-11 grid '>Welcome to Github finder</h1>
+      <h4>Search for a github user</h4>
       <UserSearch />
       <UserResults />
     </div>
